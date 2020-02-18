9359cf2d7e1d1f6a522064a0ac5312809f0c9735
Pavel Murygin <pavel.murygin@gmail.com>
2020-02-18 21:03:12 +0300

    Initial

---
+++ Hand2Note.ProgressView/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Hand2Note.ProgressView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Hand2Note.ProgressView/ProgressView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Hand2Note.ProgressView
+{
+    public partial class ProgressView : UserControl
+    {
+        public ProgressView()
+        {
+            InitializeComponent();
+        }
+    }
+}
