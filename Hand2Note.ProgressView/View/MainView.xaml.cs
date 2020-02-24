5f64206788a13809f4ea3baf7ad0ffca54e70b4f
Pavel Murygin <pavel.murygin@gmail.com>
2020-02-25 01:19:14 +0300

    Theming support

--- Hand2Note.ProgressView/View/MainView.xaml.cs
+++ Hand2Note.ProgressView/View/MainView.xaml.cs
@@ -14,6 +14,21 @@ namespace Hand2Note.ProgressView.View
                     vm => vm.DoDownloadMeVm,
                     v => v.DownloadMe.ViewModel
                 ));
+                
+                disposable(this.Bind(ViewModel,
+                    vm => vm.LightThemeChecked,
+                    v => v.LightThemeRadioBtn.IsChecked
+                ));
+                
+                disposable(this.Bind(ViewModel,
+                    vm => vm.DarkThemeChecked,
+                    v => v.DarkThemeRadioBtn.IsChecked
+                ));
+                
+                disposable(this.Bind(ViewModel,
+                    vm => vm.Theme,
+                    v => v.Style
+                ));
             });
         }
     }
--- Hand2Note.ProgressView/ViewModel/MainViewModel.cs
+++ Hand2Note.ProgressView/ViewModel/MainViewModel.cs
@@ -1,6 +1,9 @@
 // ReSharper disable UnassignedGetOnlyAutoProperty
 
+using System.Reactive.Linq;
+using System.Windows;
 using Hand2Note.ProgressView.Model.DownloadMe;
+using Hand2Note.ProgressView.Util;
 using Hand2Note.ProgressView.ViewModel.Progress;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
@@ -9,7 +12,32 @@ namespace Hand2Note.ProgressView.ViewModel
 {
     public class MainViewModel : ReactiveObject
     {
+        private readonly Style _lightTheme = (Style) Application.Current.Resources["Light"];
+        private readonly Style _darkTheme = (Style) Application.Current.Resources["Dark"];
+        
         public MainViewModel()
+        {
+            InitDownloadMeVm();
+
+            LightThemeChecked = true;
+            
+            this.WhenAnyValue(x => x.LightThemeChecked)
+                .Select(x => x ? _lightTheme : _darkTheme)
+                .ToPropertyOnMainThread(this, x => x.Theme);
+        }
+        
+        public Style Theme { [ObservableAsProperty] get; }
+
+        [Reactive]
+        public bool LightThemeChecked { get; set; }
+        
+        [Reactive]
+        public bool DarkThemeChecked { get; set; }
+        
+        [Reactive]
+        public ProgressViewModel DoDownloadMeVm { get; set; }
+
+        private void InitDownloadMeVm()
         {
             var fsm = DownloadMeFsm.Create();
             var notifications = DownloadMeProgressViewAdapter.FsmStatesToNotifications(fsm);
@@ -17,11 +45,8 @@ namespace Hand2Note.ProgressView.ViewModel
             {
                 Units = new BytesUnitInfo()
             };
-            
+
             DoDownloadMeVm = new ProgressViewModel(notifications, config, fsm.OnStart, fsm.OnPause, fsm.OnResume);
         }
-
-        [Reactive]
-        public ProgressViewModel DoDownloadMeVm { get; set; }
     }
 }
